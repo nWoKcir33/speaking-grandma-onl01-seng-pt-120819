473cc31807254b02f4610573029b8728b54bd7e1
Rick Lim <rick.jason.a.lim@gmail.com>
2019-12-11 23:26:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -19,9 +19,10 @@
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+    elsif string == string.upcase
+    return "NO, NOT SINCE 1938!"
   elsif string != string.upcase
     return "HUH?! SPEAK UP, SONNY!"
+    
   end
 end
-
-puts speak_to_grandma('I LOVE YOU GRANDMA!')
